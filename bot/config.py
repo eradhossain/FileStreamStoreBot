d50a692a8f43dff960f7958886896f439acb4445
eradhossain <98438334+eradhossain@users.noreply.github.com>
2024-10-04 08:04:30 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,12 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", ))
-    API_HASH = env.get("TELEGRAM_API_HASH", "")
+    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
+    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
     OWNER_ID = int(env.get("OWNER_ID", 1119833396))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
     BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "FilesDirectBot")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
     CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001694910892))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
